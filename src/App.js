08a1f64e752faa19ae05b84e619516fbc534caef
jaeuk Lee <lskkhj1005@naver.com>
2023-02-06 18:47:35 +0900

    add, delete button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 function App() {
   // 초기값과 setTodo 만들기
   const [todo, setTodo] = useState([
-    { id: 1, title: "리액트 공부하기", contents: "리액트 기초 공부", done: false},
-    { id: 2, title: "리액트 공부합시다", contents: "노션 보기", done: false},
-    { id: 3, title: "리액트 공부하기", contents: "리액트 기초 공부", done: false},
+    { id: 1, title: "리액트 공부하기", contents: "리액트 기초 공부", done: false },
+    { id: 2, title: "리액트 공부합시다", contents: "노션 보기", done: false },
+    { id: 3, title: "리액트 공부하기", contents: "리액트 기초 공부", done: false },
   ])
 
   const [title, setTitle] = useState('')
-  const [contents ,setContents] = useState('')
+  const [contents, setContents] = useState('')
 
+  // input에 작성하는 대로 렌더링
   const titleChangeHandler = (event) => {
     setTitle(event.target.value);
   }
@@ -20,23 +21,69 @@ function App() {
     setContents(event.target.value);
   }
 
+  // 추가 버튼
+  const clickAddbutton = () => {
+    const newTodo = {
+      id: todo.length + 1,
+      title,
+      contents,
+      done: false,
+    }
+
+    // 
+    setTodo([...todo, newTodo]);
+
+    // 작성 후 input 안에 있는 값 초기화
+    setTitle('')
+    setContents('')
+  }
+
+  // 삭제 버튼
+  const clickRomoveButton = (id) => {
+    const newTodo = todo.filter(function(todo) {
+      return todo.id !== id
+    });
+    setTodo(newTodo)
+  }
+
   return (
     <div>
       <h1>My Todo List</h1>
       <div>
+        {/* input 태그들 */}
         제목
-        <input 
+        <input
           value={title}
           onChange={titleChangeHandler}
         >
 
         </input>
-        내용 <input 
+        내용 <input
           value={contents}
           onChange={contentsChangeHandler}
         >
         </input>
-        <button onClick={}>추가하기!</button>
+        <button onClick={clickAddbutton}>추가하기!</button>
+      </div>
+
+
+      {/* Done */}
+      <div>
+        <h1>Working...</h1>
+        {
+          todo.map(item => {
+            return (
+              <div key={item.id} className="">
+                <h3>{item.title}</h3>
+                <p>{item.contents}</p>
+
+                <button onClick={() => clickRomoveButton(item.id)}>삭제하기!</button>
+
+                <button>완료!</button>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
